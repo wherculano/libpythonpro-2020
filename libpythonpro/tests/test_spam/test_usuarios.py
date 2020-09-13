f9335ca165d0bf014bd87909e5a650438ba7b400
wherculano <wagherculano@hotmail.com>
2020-09-13 20:24:38 -0300

    Configurada a fixture para a utilizacao do Setup e Tear Down

--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -1,25 +1,36 @@
+import pytest
+
 from libpythonpro.spam.db import Conexao
 from libpythonpro.spam.modelos import Usuario
 
 
-def test_salvar_usuario():
-    conexao = Conexao()  # gerencia a autenticação (login e senha)
-    sessao = conexao.gerar_sessao()  # efetua as operações (CRUD)
+@pytest.fixture
+def conexao():
+    #  Setup
+    conexao_obj = Conexao()  # gerencia a autenticação (login e senha)
+    yield conexao_obj
+    # Tear Down
+    conexao_obj.fechar()
+
+
+@pytest.fixture
+def sessao(conexao):
+    # Setup
+    sessao_obj = conexao.gerar_sessao()  # efetua as operações (CRUD)
+    yield sessao_obj
+    #  Tear Down
+    sessao_obj.roll_back()
+    sessao_obj.fechar()
+
+
+def test_salvar_usuario(sessao):
     usuario = Usuario(nome="Wagner")
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
-    sessao.roll_back()
-    sessao.fechar()
-    conexao.fechar()
 
 
-def test_listar_usuario():
-    conexao = Conexao()
-    sessao = conexao.gerar_sessao()
+def test_listar_usuario(sessao):
     usuarios = [Usuario(nome="Wagner"), Usuario(nome="Herculano")]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
-    sessao.roll_back()
-    sessao.fechar()
-    conexao.fechar()
