86548cbea608c1ebb00242c609e3b6144d3bb2d2
wherculano <wagherculano@hotmail.com>
2020-09-14 08:26:29 -0300

    Efetuada refatoração para testar a quantidade de emails enviados

--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -2,13 +2,14 @@ from libpythonpro.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome="Wagner")
+    usuario = Usuario(nome="Wagner", email="wagherculano@hotmail.com")
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuario(sessao):
-    usuarios = [Usuario(nome="Wagner"), Usuario(nome="Herculano")]
+    usuarios = [Usuario(nome="Wagner", email="wagherculano@hotmail.com"),
+                Usuario(nome="Herculano", email="wagherculano@hotmail.com")]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
--- libpythonpro/tests/test_spam/teste_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/teste_envio_para_base_de_usuarios.py
@@ -1,11 +1,30 @@
+import pytest
+
 from libpythonpro.spam.enviador_de_email import Enviador
 from libpythonpro.spam.main import EnviadorDeSpam
+from libpythonpro.spam.modelos import Usuario
 
 
-def test_envio_de_spam(sessao):
-    enviador_de_spam = EnviadorDeSpam(sessao, Enviador())
+@pytest.mark.parametrize(
+    'usuarios',
+    [
+        [
+            Usuario(nome="Wagner", email="wagherculano@hotmail.com"),
+            Usuario(nome="Herculano", email="wagherculano@hotmail.com")
+        ],
+        [
+            Usuario(nome="Wagner", email="wagherculano@hotmail.com"),
+        ],
+    ]
+)
+def test_envio_de_spam(sessao, usuarios):
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    enviador = Enviador()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'wagherculano@hotmail.com',
         'Curso Python Pro',
         'Confira os módulos fantásticos!'
     )
+    assert len(usuarios) == enviador.qtde_emails_enviados
