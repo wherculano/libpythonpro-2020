a3a8e672fc47443b2b6095aa7d471802e681dd0a
wherculano <wagherculano@hotmail.com>
2020-09-15 15:12:53 -0300

    Adicionada pytest-mock close #19

--- libpythonpro/tests/test_spam/test_github_api.py
+++ libpythonpro/tests/test_spam/test_github_api.py
@@ -6,16 +6,16 @@ from libpythonpro import github_api
 
 
 @pytest.fixture
-def avatar_url():
+def avatar_url(mocker):
     resp_mock = Mock()
     url = 'https://avatars2.githubusercontent.com/u/26460999?v=4'
     resp_mock.json.return_value = {'login': 'wherculano', 'id': 26460999, 'node_id': 'MDQ6VXNlcjI2NDYwOTk5',
                                    'avatar_url': url,
                                    }
-    get_original = github_api.requests.get
-    github_api.requests.get = Mock(return_value=resp_mock)
-    yield url
-    github_api.requests.get = get_original
+
+    get_mock = mocker.patch('libpythonpro.github_api.requests.get')
+    get_mock.return_value = resp_mock
+    return url
 
 
 def test_buscar_avatar(avatar_url):
